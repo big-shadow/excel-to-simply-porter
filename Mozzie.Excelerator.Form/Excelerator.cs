2850ba1e11ba2d5ef37bc0161d3f592de5bf70fd
Ray Winkelman <raywinkelman@gmail.com>
2018-02-09 16:37:01 -0400

    Adds excel interlop

--- Mozzie.Excelerator.Form/Excelerator.cs
+++ Mozzie.Excelerator.Form/Excelerator.cs
@@ -1,6 +1,12 @@
-﻿namespace Mozzie.Excelerator
+﻿using Excel = Microsoft.Office.Interop.Excel;
+
+namespace Mozzie.Excelerator
 {
     internal class Excelerator
     {
+        public Excelerator(){
+            var spreadsheet = new Excel.Application();
+            spreadsheet.Visible = false;
+        }
     }
 }
